7e4eef4d4ead7b350616e4ca5d5d7a7715cc4ff9
AWS-Altan <50998297+AWS-Altan@users.noreply.github.com>
2020-09-14 13:08:21 -0500

    Add Header and Footer

--- View/PlantillaInicio.php
+++ View/PlantillaInicio.php
@@ -1,106 +1,25 @@
-<html>
-<title>Portal ADD</title>
-<link rel="stylesheet" href="Style/header.css">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <style>
-     #mitabla{
-/*estilo de el cuerpo de nuestra tabla*/
-background-color: #1e8cbe;
-width:80%;
-margin:24px auto;
-border-left: 1px solid #ccc;
-border-right: 1px solid #ccc;
-font-family:helvetica,arial,sans-serif;
-font-weight:normal;
-text-transform: uppercase;
-}
-
-#mitabla th{
-/*estilo de los elementos(th)*/
-background-color: #278dbc;
-color:#fff;
-font-weight: normal;
-font-size:14px;
-padding: 8px;
-}
-
-#mitabla td{
-
-/*estilo a los elementos (td)*/
-width:100px;
-height: 24px;
-}
-
-
-
-#mitabla tr:nth-child(odd){
-
-background-color: #ffffff;
-}
-
-#mitabla tr:nth-child(even){
-
-/*para los colores de las filas pares*/
-background:#EDEDED;
-}
-* {
-	margin:0px;
-	padding:0px;
-}
-
-#header {
-	margin:auto;
-	width:500px;
-	font-family:Arial, Helvetica, sans-serif;
-}
-
-ul, ol {
-	list-style:none;
-}
-
-.nav > li {
-	float:center;
-}
-
-.nav li a {
-	background-color:#000;
-	color:#fff;
-	text-decoration:none;
-	padding:10px 12px;
-	display:block;
-}
-
-.nav li a:hover {
-	background-color:#434343;
-}
-
-.nav li ul {
-	display:none;
-	position:absolute;
-	min-width:140px;
-}
-
-.nav li:hover > ul {
-	display:block;
-}
-
-.nav li ul li {
-	position:relative;
-}
-
-.nav li ul li ul {
-	right:-140px;
-	top:100px;
-}
-    </style>
-<?php 
- echo InicioController::headers(); 
-?>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/util.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="bootstrap/js/bootstrap.min.js">
+    <link rel="stylesheet" href="jquery/jquery-3.5.1.min.js">
+    <link rel="stylesheet"  type="text/css" href="datatables/DataTables-1.10.21/css/dataTables.bootstrap4.min.css"> 
+    <link rel="stylesheet" type="text/css" href="datatables/datatables.min.css"/>
+    <title>Document</title>
 </head>
 <body>
-
 <?php
-   echo InicioController::enlacesPagina();  
+include_once "View/modules/header.php";
+
+echo InicioController::enlacesPagina(); 
+
+include_once "View/modules/footer.php";
 ?>
 </body>
 </html>
--- View/modules/header.php
+++ View/modules/header.php
@@ -1,33 +1,59 @@
 
-<nav id="header">
-	<ul  class="nav">
-	<li><a href="index.php?list=beid">Beids</a>
-	<ul class="submenu">
-						<li><a href="index.php?list=beid">Consulta</a>
-						<li><a href="index.php?action=beid">Alta</a></li>
-						<li><a href="index.php?action=delbeid">Baja</a></li>
-					</ul>
-				</li>
-				
-	<li><a href="index.php?list=apn">APNS</a>
-	<ul class="submenu">
-						<li><a href="index.php?list=apn">Consulta</a>
-						<li><a href="index.php?action=apn">Alta</a></li>
-						<li><a href="index.php?action=delapn">Baja</a></li>
-					</ul>
-				</li>
-				
-	<li><a href="index.php?list=spn">SPNS</a>
-	<ul class="submenu">
-	<li><a href="index.php?list=spn">Consulta</a>
-						<li><a href="index.php?action=spn">Alta</a></li>
-						<li><a href="index.php?action=delspn">Baja</a></li>
-					</ul>
-				</li>
-				
-    <li><a href="index.php?action=registro">Registrar</a></li>
-	</ul>
-</nav>
-</br>
-</br>
-</br>
+<div class="d-flex" id="content-wrapper">
+  <div id="sidebar-container" class="bg-primary">
+            <div class="logo">
+                <h4 class="text-light font-weight-bold mb-0">ADD</h4>
+            </div>
+            <div class="menu">
+                <a href="#" class="d-block text-light p-3 border-0">
+                    BEIDs</a>
+
+                <a href="#" class="d-block text-light p-3 border-0">
+                    APNS</a>
+
+                <a href="#" class="d-block text-light p-3 border-0">
+                  SPNS</a>
+                <a href="#" class="nav-link  dropdown-toggle d-block text-light p-3 border-0 " href="#" id="navbarDropdown" role="button"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Mensajeria</a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="#">Barring</a>
+                      <a class="dropdown-item" href="#">Bienvenida</a>
+                      <a class="dropdown-item" href="#">Portabilidad</a>
+                      <a class="dropdown-item" href="#">Cambio DN</a>
+                    </div>
+                <a href="#" class="d-block text-light p-3 border-0">
+                    Consulta de saldo</a>
+            </div>
+        </div>
+        <!-- Fin sidebar -->
+
+        <div class="w-100">
+
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+            <div class="container">
+    
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+    
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                
+                <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                  <li class="nav-item dropdown">
+                    <a class="nav-link text-dark dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                      data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Diego Velázquez
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="#">Registrar</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="#">Cerrar sesión</a>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
