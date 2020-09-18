f16be2799057d72ef5a6ce9b249561ebb478f342
AWS-Altan <50998297+AWS-Altan@users.noreply.github.com>
2020-09-18 11:45:16 -0500

    add

--- View/PlantillaInicio.php
+++ View/PlantillaInicio.php
@@ -3,14 +3,12 @@
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style/lmain.css">
-    <link rel="stylesheet" href="style/util.css">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/util.css">
     <link rel="stylesheet" href="style/style.css">
-    <link rel="stylesheet" href="bootstrap/js/bootstrap.min.js">
-    <link rel="stylesheet" href="jquery/jquery-3.5.1.min.js">
-    <link rel="stylesheet"  type="text/css" href="datatables/DataTables-1.10.21/css/dataTables.bootstrap4.min.css"> 
     <link rel="stylesheet" type="text/css" href="datatables/datatables.min.css"/>
+    <link rel="stylesheet"  type="text/css" href="datatables/DataTables-1.10.18/css/dataTables.bootstrap4.min.css"> 
     <title>Document</title>
 </head>
 <body>
